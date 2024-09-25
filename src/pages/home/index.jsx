df34fcc27af64e4132b5ba3633c4c756cfeabd36
Paulo Henrique de Paula Dias <141594243+PAULOHENRIQUEDEPAULADIAS@users.noreply.github.com>
2024-09-25 18:23:09 -0300

    Scond Commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Home(){
-
     return (
         <div>
             <h2>Hello Home</h2>
+
+            <Link to="/products">Ir para a próxima tela</Link>
         </div>
     );
 }
